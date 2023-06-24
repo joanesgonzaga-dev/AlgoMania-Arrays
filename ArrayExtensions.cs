33c700d11b9dca3f422d3e0ad0334e6eb8145f36
Joanes Gonzaga <joanesgonzaga.dev@gmail.com>
2023-06-24 13:22:08 -0300

    First versioning commit

---
+++ ArrayExtensions.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoMania_Arrays
+{
+    public static class ArrayExtensions
+    {
+        public static void RemoveAllEquals<T>(this T[] _array, T item, ref T[] myArray) //
+        {
+            // O(n)
+            List<T> newArray = new List<T>();
+            
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                if (!myArray[i].Equals(item))
+                {
+                    newArray.Add(myArray[i]);
+                }  
+            }
+            myArray = newArray.ToArray();
+        }
+
+        public static void RemoveFirstEquals<T>(this T[] _array, T item, ref T[] myArray)
+        {
+            // O(n)
+            List<T> newArray = new List<T>();
+            bool firstEquals = true;
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                if (!myArray[i].Equals(item))
+                {
+                    newArray.Add(myArray[i]);
+                }
+                else if(myArray[i].Equals(item) & firstEquals){
+                    
+                    firstEquals = false;
+                }
+                else
+                {
+                    newArray.Add(myArray[i]);
+                }   
+            }
+            myArray = newArray.ToArray();
+        }
+
+        public static T ReturnsRepeater<T>(this T[] _array)
+        {
+            //O(n^2)
+            int maximumRepeater = 0;
+            T repeaterItem = _array[0];
+
+            for (int i = 0; i < _array.Length; i++)
+            {
+                int currentRepeater = 0;
+
+                for (int j = 0; j < _array.Length; j++)
+                {
+                    if (_array[i].Equals(_array[j]))
+                    {
+                        currentRepeater++;
+                    }
+                    if (currentRepeater > maximumRepeater)
+                    {
+                        repeaterItem = _array[i];
+                        maximumRepeater = currentRepeater;
+                    }
+                }      
+            }
+            return repeaterItem;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoMania_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = new double[] { 9.7, 3.1, 1.2, 4.7, 15.00,15.00,15.00,15.00,6.5, 8.0, 0.0, 2.01, 5.8, 7.9, 8.0 };
+
+            Console.Write("{");
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(String.Format(new CultureInfo("en-US"), "{0:0.00}", numbers[i]) + (i < numbers.Length - 1 ? "," : ""));
+            }
+
+            Console.Write("}");
+            Console.WriteLine();
+
+            double d = numbers.ReturnsRepeater<double>();
+
+            Console.WriteLine("No Array acima, o elemento que mais se repete é: " + String.Format(new CultureInfo("en-US"), "{0:0.00}", d));
+            Console.Read();
+        }
+
+        private static void RemoveItems()
+        {
+            double[] numbers = new double[] { 9.7, 3.1, 1.2, 4.7, 6.5, 8.0, 0.0, 2.01, 5.8, 7.9, 8.0 };
+            Array.Sort(numbers);
+            numbers[3] = 12;
+
+            var _app = numbers.Append<double>(50.4);
+
+            numbers.RemoveFirstEquals<double>(8.0, ref numbers);
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
+            Console.WriteLine("------------------------------------------");
+
+            for (int i = 0; i < _app.Count(); i++)
+            {
+                Console.WriteLine(_app.ElementAt(i));
+            }
+        }
+    }
+}
